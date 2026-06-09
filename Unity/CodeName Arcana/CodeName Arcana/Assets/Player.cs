0d001097c36c33ee9fac6508c6ac75078d1065c3
nima <nimamalekpour1386@gmail.com>
2026-06-09 22:43:12 +0330

    the player script got edited
    basic movments with keyboard added

--- Unity/CodeName Arcana/CodeName Arcana/Assets/Player.cs
+++ Unity/CodeName Arcana/CodeName Arcana/Assets/Player.cs
@@ -1,93 +1,34 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float health = 100;
-    public Image health_image;
-    Rigidbody2D myRig;
-    public Animator cam_anim;
-    Animator player_anim;
-    public float jump_rate, speed;
-    public GameObject myCamera;
-    public float speed_camera;
-    public GameObject umbrella_on, umbrella_off;
-    public bool is_jump, live;
-    public GameObject background;
+    public float speed;
     void Start()
     {
-        myRig = GetComponent<Rigidbody2D>();
-        player_anim = GetComponent<Animator>();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-
-
-        if (health <= 0)
-        {
-            live = false;
-        }
-        else
-        {
-            live = true;
+        if (Input.GetKey(KeyCode.W))
+        {  
+            transform.Translate (new Vector3(0, 0, speed * Time.deltaTime));
         }
 
-        health_image.fillAmount = health / 100;
-
-        if (live)
+        if (Input.GetKey(KeyCode.S))
         {
-            if (Input.GetKey(KeyCode.Space) || is_jump == true)
-            {
-                player_anim.SetBool("up", true);
-                myRig.linearVelocity = new Vector2(myRig.linearVelocity.x, jump_rate);
-                umbrella_on.SetActive(true);
-                umbrella_off.SetActive(false);
-            }
-            else if (is_jump == false)
-            {
-                player_anim.SetBool("up", false);
-                umbrella_off.SetActive(true);
-                umbrella_on.SetActive(false);
-            }
-
-            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, 0, - speed * Time.deltaTime));
         }
 
-        background.transform.position = new Vector2(transform.position.x, background.transform.position.y);
-        myCamera.transform.position = new Vector3(transform.position.x, myCamera.transform.position.y, myCamera.transform.position.z);
-
-        if (transform.position.y > 0.5f)
+        if (Input.GetKey(KeyCode.D))
         {
-            myCamera.transform.position = new Vector3(myCamera.transform.position.x, Mathf.Lerp(myCamera.transform.position.y, 3.4f, speed_camera * Time.deltaTime), myCamera.transform.position.z);
-            cam_anim.SetBool("zoom", true);
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
-        else
+
+        if (Input.GetKey(KeyCode.A))
         {
-            myCamera.transform.position = new Vector3(myCamera.transform.position.x, Mathf.Lerp(myCamera.transform.position.y, 0, speed_camera * Time.deltaTime), myCamera.transform.position.z);
-            cam_anim.SetBool("zoom", false);
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
     }
-
-    public void Click_Donw()
-    {
-        Debug.Log("Click_Donw");
-        is_jump = true;
-    }
-
-    public void Click_Up()
-    {
-        Debug.Log("Click_Up");
-        is_jump = false;
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "enemy")
-        {
-            health -= 10;
-        }
-    }
-
-
 }
